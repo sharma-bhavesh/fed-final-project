38eb73b7d978cdb7fbf23bae7bd87bf7eba30774
sharma-bhavesh <sharmabhavesh@gmail.com>
2015-04-14 18:17:28 -0400

    Add Properties with show/hide option

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,5 +3,20 @@
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
+
+    // initialize
+    $("#form-add-property").hide();
+
+    $("#btn-add-property").on("click", function() {
+    	$(this).hide();
+    	$("#form-add-property").show();
+    })
+
+    $("#btn-cancel-add-property").on("click", function() {
+    	$("#btn-add-property").show();
+    	$("#form-add-property").hide();
+    })
+
+
   });
 })(jQuery);
